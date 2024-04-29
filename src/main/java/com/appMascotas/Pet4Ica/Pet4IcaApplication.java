9cc04b27fc458e2b72629cda02274fc994aba7c7
Alexis Diaz <alexisshande@gmail.com>
2024-04-29 17:32:13 -0500

    Creacion del backend

---
+++ src/main/java/com/appMascotas/Pet4Ica/Pet4IcaApplication.java
@@ -0,0 +1,13 @@
+package com.appMascotas.Pet4Ica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pet4IcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pet4IcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appMascotas/Pet4Ica/Pet4IcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appMascotas.Pet4Ica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pet4IcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
